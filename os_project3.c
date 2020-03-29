85fad78b7487071d69bca8e79885f55a71ea4267
akshat3362 <49157243+akshat3362@users.noreply.github.com>
2020-03-29 22:16:37 +0530

    Update os_project3.c

--- os_project3.c
+++ os_project3.c
@@ -11,10 +11,10 @@ void student()
 int k,i,total;
 for(i=0;i<q.sum;i++)
 {
-	printf("enter the burst time for the %d query(in minutes)\n",(i+1));
+	printf("enter the burst time for the student %d query(in minutes)\n",(i+1));
 	scanf("%d",&q.student_burst_time[i]);
 	printf("ENTER THE ARRIVAL TIME IN 24 HOURS FORMAT AND ENTER TIME BETWEEN 10 AND 12 ONLY . EG- FOR 10:00 ENTER 1000\n");
-	printf("enter the arrival time for the %d query\n",(i+1));
+	printf("enter the arrival time for the student %d query\n",(i+1));
 	scanf("%d",&q.student_arrival_time[i]);
 	if(q.student_arrival_time[i]>=1000 & q.student_arrival_time[i]<=1200)
 	{
@@ -34,9 +34,9 @@ void faculty()
 int j,i;
 for(j=0;j<q.sum1;j++)
 {
-	printf("enter the burst time for the %d query",(j+1));
+	printf("enter the burst time for the  faculty %d query",(j+1));
 	scanf("%d",&q.faculty_burst_time[j]);
-	printf("enter the arrival time for the %d query\n",(j+1));
+	printf("enter the arrival time for the  faculty %d query\n",(j+1));
 	printf("ENTER THE TIME IN 24 HOURS FORMAT. EG- FOR 10:00 ENTER 1000\n");
 	scanf("%d",&q.faculty_arrival_time[j]);
 	if(q.faculty_arrival_time[j]>=1000 & q.faculty_arrival_time[j]<=1200)
@@ -55,18 +55,39 @@ for(j=0;j<q.sum1;j++)
 }
 void schedule()
 {
-	int temp,ttime;
+	int temp,ttime=0;
+if(q.faculty_arrival_time[0]!=0 & q.student_arrival_time[0]!=0)
+{	
 if(q.student_arrival_time[0]<q.faculty_arrival_time[0])
 {
 temp=q.student_arrival_time[0];	
 }
-else
+else if(q.student_arrival_time[0]>q.faculty_arrival_time[0])
 {
 	temp=q.faculty_arrival_time[0];
+}
+else
+{
+	temp=q.student_arrival_time[0];
+}
+}
+else
+{
+	if(q.faculty_arrival_time[0]>q.student_arrival_time[0])
+	{
+		temp=q.faculty_arrival_time[0];
+	
+	}
+	else
+	{
+		temp=q.student_arrival_time[0];
+	
+	}
 }
 int j=0,k=0,i=0,quantum;
 printf("\nenter the quantum\n");
 scanf("%d",&quantum);
+printf("\nPROCESS NUMBER \t REMAINING TIME\t \tTIME");
 while(k<(q.sum+q.sum1))
 {
 for(j=0;j<q.sum1;j++)
@@ -77,7 +98,8 @@ if(temp>=q.faculty_arrival_time[j])
 	if(q.faculty_burst_time[j]>0)
     { 
 if(q.faculty_burst_time[j]>=quantum)
-    { 
+    {
+	printf("\n\t\t\t\t\t%d-",temp); 
 temp+=quantum;
 ttime+=quantum;
 q.faculty_burst_time[j]=q.faculty_burst_time[j]-quantum;
@@ -87,6 +109,7 @@ k++;
    }
 else 
 { 
+printf("\n\t\t\t\t\t%d-",temp); 
 temp+=q.faculty_burst_time[j];
 ttime+=q.faculty_burst_time[j];
 q.faculty_burst_time[j]=0;
@@ -106,6 +129,7 @@ if(temp>=q.student_arrival_time[i])
     { 
 if(q.student_burst_time[i]>=quantum)
     { 
+    printf("\n\t\t\t\t\t%d-",temp); 
 temp+=quantum;
 ttime+=quantum;
 q.student_burst_time[i]=q.student_burst_time[i]-quantum;
@@ -115,6 +139,7 @@ k++;
    }
 else 
 { 
+printf("\n\t\t\t\t\t%d-",temp); 
 temp+=q.student_burst_time[i];
 ttime+=q.student_burst_time[i];
 q.student_burst_time[i]=0;
@@ -135,10 +160,14 @@ else
 }
 }
 }
+printf("\nTotal Time spent on handling queries %d\n",ttime);
+float e = (q.sum+q.sum1);
+printf("Average Time spent on handling queries %f\n",(ttime/e));
 }
 int main_page()
 {
 	int i,process_number;
+	q.sum=0,q.sum1=0;
 	printf("enter the number of processes");
 	scanf("%d",&process_number);
 	int query_number[process_number];
